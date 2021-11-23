ecacb3e8e68732be4311be176d3cb1d23eacb37c
Faceguy <kattreinkyle@gmail.com>
2021-11-23 01:30:38 -0500

    Search debounce, clear button works

--- src/components/Shuffle/ShuffleCollection.js
+++ src/components/Shuffle/ShuffleCollection.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useCallback, useMemo} from "react";
 import {debounce} from 'lodash';
 import Select from "react-select";
 import "react-select/dist/react-select.cjs";
@@ -9,252 +9,267 @@ import tomes from "./tomes.json"
 import uniques from "./uniques.json"
 import scrolls from "./scrolls.json"
 import McText from "mctext-react/lib/McText";
+import {DebounceInput} from "react-debounce-input";
 
 export const ShuffleCollection = () => {
 
-    const [cardItems, setCardItems] = useState([]);
-    const [filteredItems, setFilteredItems] = useState([{name: "loading", meme: "yes"}]);
+  const [cardItems, setCardItems] = useState([]);
+  const [filteredItems, setFilteredItems] = useState([{name: "loading", meme: "yes"}]);
 
-    const [availableTags, setAvailableTags] = useState([]);
+  const [availableTags, setAvailableTags] = useState([]);
 
-    const [selectedTags, setSelectedTags] = useState([]);
-    const [selectedType, setSelectedType] = useState({value: "gem", label: "Gems"});
-    const [selectedRarity, setSelectedRarity] = useState();
-    const [searchText, setSearchText] = useState();
+  const [selectedTags, setSelectedTags] = useState([]);
+  const [selectedType, setSelectedType] = useState({value: "gem", label: "Gems"});
+  const [selectedRarity, setSelectedRarity] = useState();
+  const [searchText, setSearchText] = useState();
 
-    const [filterTags, setFilterTags] = useState([]);
+  const [filterTags, setFilterTags] = useState([]);
 
-    const bannerText = {};
-    bannerText.event = "Event Reward"
-    bannerText.transmute = "Transmutation Only"
-    bannerText.discontinued = "Discontinued"
+  const bannerText = {};
+  bannerText.event = "Event Reward"
+  bannerText.transmute = "Transmutation Only"
+  bannerText.discontinued = "Discontinued"
 
-    const typeOptions = [
-        {value: "any", label: "Any"},
-        {value: "gem", label: "Gems"},
-        {value: "tome", label: "Tomes"},
-        {value: "scroll", label: "Scrolls"},
-        {value: "unique", label: "Uniques"}
-    ]
+  const typeOptions = [
+    {value: "any", label: "Any"},
+    {value: "gem", label: "Gems"},
+    {value: "tome", label: "Tomes"},
+    {value: "scroll", label: "Scrolls"},
+    {value: "unique", label: "Uniques"}
+  ]
 
-    const rarityOptions = [
-        {value: "any", label: "Any"},
-        {value: "Common", label: "Common"},
-        {value: "Uncommon", label: "Uncommon"},
-        {value: "Rare", label: "Rare"},
-        {value: "Epic", label: "Epic"}
-    ]
+  const rarityOptions = [
+    {value: "any", label: "Any"},
+    {value: "Common", label: "Common"},
+    {value: "Uncommon", label: "Uncommon"},
+    {value: "Rare", label: "Rare"},
+    {value: "Epic", label: "Epic"}
+  ]
 
-    useEffect(() => {
-        if (cardItems.length === 0) {
-            const newItems = [];
-            for (const [key, value] of Object.entries(gems)) {
-                const item = value;
-                item.type = "gem"
-                item.img = "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/26/Emerald_JE3_BE3.png";
-                item.background = "#10c810";
-                newItems.push(item);
-            }
-            for (const [key, value] of Object.entries(tomes)) {
-                const item = value;
-                item.type = "tome";
-                item.img = "https://static.wikia.nocookie.net/minecraft_gamepedia/images/5/50/Book_JE2_BE2.png";
-                item.background = "#1243d9";
-                newItems.push(item);
-            }
-            for (const [key, value] of Object.entries(uniques)) {
-                const item = value;
-                item.type = "unique";
-                item.img = "https://static.wikia.nocookie.net/minecraft_gamepedia/images/5/50/Book_JE2_BE2.png";
-                item.background = "#d99712";
-                newItems.push(item);
-            }
-            for (const [key, value] of Object.entries(scrolls)) {
-                const item = value;
-                item.type = "scroll";
-                item.img = "https://static.wikia.nocookie.net/minecraft_gamepedia/images/f/f2/Paper_JE2_BE2.png";
-                item.background = "#34981a";
-                newItems.push(item);
-            }
-            setCardItems(newItems);
-            setSelectedTags([]);
-        }
-    }, []);
-
-    useEffect(() => {
-        applyFilters()
-    }, [searchText, selectedTags, selectedRarity, selectedType]);
-
-    useEffect(() => {
-        const set = new Set(availableTags);
-        cardItems.forEach((item) => {
-            item.groupNames.forEach((tag) => {
-                set.add(tag);
-            })
-        });
-        setAvailableTags(Array.from(set));
-
-        const tags = []
-        set.forEach((tag) => {
-            tags.push({value: tag, label: tag})
-        });
-        setFilterTags(tags);
-    }, [cardItems]);
-
-    const applyFilters = () => {
-        setFilteredItems(cardItems.filter(item => itemMatchesFilters(item) && itemMatchesSearch(item)));
+  useEffect(() => {
+    if (cardItems.length === 0) {
+      const newItems = [];
+      for (const [key, value] of Object.entries(gems)) {
+        const item = value;
+        item.type = "gem"
+        item.img = "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/26/Emerald_JE3_BE3.png";
+        item.background = "#10c810";
+        newItems.push(item);
+      }
+      for (const [key, value] of Object.entries(tomes)) {
+        const item = value;
+        item.type = "tome";
+        item.img = "https://static.wikia.nocookie.net/minecraft_gamepedia/images/5/50/Book_JE2_BE2.png";
+        item.background = "#1243d9";
+        newItems.push(item);
+      }
+      for (const [key, value] of Object.entries(uniques)) {
+        const item = value;
+        item.type = "unique";
+        item.img = "https://static.wikia.nocookie.net/minecraft_gamepedia/images/5/50/Book_JE2_BE2.png";
+        item.background = "#d99712";
+        newItems.push(item);
+      }
+      for (const [key, value] of Object.entries(scrolls)) {
+        const item = value;
+        item.type = "scroll";
+        item.img = "https://static.wikia.nocookie.net/minecraft_gamepedia/images/f/f2/Paper_JE2_BE2.png";
+        item.background = "#34981a";
+        newItems.push(item);
+      }
+      setCardItems(newItems);
+      setSelectedTags([]);
     }
+  }, []);
 
-    const itemMatchesFilters = (item) => {
-        if (selectedRarity && selectedRarity.value !== "any" && item.rarity !== selectedRarity.value) {
-            return false;
-        }
-        if (selectedType && selectedType.value !== "any" && item.type !== selectedType.value) {
-            return false;
-        }
-        if (selectedTags.length > 0 && !tagsApplicable(item)) {
-            return false;
-        }
-        return true
+  useEffect(() => {
+    applyFilters()
+  }, [searchText, selectedTags, selectedRarity, selectedType]);
+
+  useEffect(() => {
+    const set = new Set(availableTags);
+    cardItems.forEach((item) => {
+      item.groupNames.forEach((tag) => {
+        set.add(tag);
+      })
+    });
+    setAvailableTags(Array.from(set));
+
+    const tags = []
+    set.forEach((tag) => {
+      tags.push({value: tag, label: tag})
+    });
+    setFilterTags(tags);
+  }, [cardItems]);
+
+  const applyFilters = () => {
+    setFilteredItems(
+      cardItems.filter(item => itemMatchesFilters(item) && tagsApplicable(item) && itemMatchesSearch(item))
+    );
+  }
+
+  const itemMatchesFilters = (item) => {
+    if (selectedRarity && selectedRarity.value !== "any" && item.rarity !== selectedRarity.value) {
+      return false;
     }
-
-    const tagsApplicable = (item) => {
-        if (selectedTags.length === 0) return true;
-        for (const tag of selectedTags) {
-            if (!item.groupNames.includes(tag.value)) return false
-        }
-        return true;
+    if (selectedType && selectedType.value !== "any" && item.type !== selectedType.value) {
+      return false;
     }
+    return true
+  }
 
-    const itemMatchesSearch = (item) => {
-        return tagsApplicable(item) && (!searchText ||
-            item.name?.toLowerCase().includes(searchText) ||
-            item.title?.toLowerCase().includes(searchText) ||
-            item.description?.join(" ").toLowerCase().includes(searchText) ||
-            item.groupNames?.join(" ").toLowerCase().includes(searchText))
+  const tagsApplicable = (item) => {
+    if (selectedTags.length === 0) return true;
+    for (const tag of selectedTags) {
+      if (!item.groupNames.includes(tag.value)) return false
     }
+    return true;
+  }
 
-    const forceTag = (tag) => {
-        /*
-        if (searchTags.includes(tag)) return;
-        const newTags = Object.assign([], searchTags);
-        newTags.push(tag);
-        setSearchTags(newTags);
-        setSearchText("");
-        setFilter({...filter, type: undefined})
-        */
+  const itemMatchesSearch = (item) => {
+    if (!searchText) {
+      return true;
     }
-
-    const filterSection = (
-        <div className="width100 flexRow align-left padding-full-10">
-            <div className="width40 margin-full-5">
-                <Select
-                    placeholder="Item Tags"
-                    isMulti
-                    value={selectedTags}
-                    options={filterTags}
-                    onChange={setSelectedTags}
-                />
-            </div>
-            <div className="width15 margin-full-5">
-                <Select
-                    placeholder="Type"
-                    value={selectedType}
-                    options={typeOptions}
-                    onChange={setSelectedType}
-                />
-            </div>
-            <div className="width15 margin-full-5">
-                <Select
-                    placeholder="Rarity"
-                    value={selectedRarity}
-                    options={rarityOptions}
-                    onChange={setSelectedRarity}
-                />
-            </div>
-            <div className="width20 margin-full-5 searchBox">
-                <input
-                    placeholder="Search"
-                    onChange={debounce((e) => setSearchText(e.target.value.toLowerCase()), 1000)}
-                />
-            </div>
-            <div className="width5 margin-full-5 clearButton">
-                <button onClick={debounce((e) => setSearchText(e.target.value.toLowerCase()), 1000)}>X</button>
-            </div>
-        </div>
-    )
-
-    const yeHaplessBuffoon = (
-        <div className="shuffleCard" style={{borderColor: "#AA0000"}} key="invalid-search">
-            <div className="shuffleContent">
-                <div className="shuffleElement">
-                    <div className="title">⚠ IMPOTENT QUERIER DETECTED ⚠</div>
-                    <div className="subtitle">HALT! YOU'VE FOUND NO RESULTS!</div>
-                </div>
-                <div className="shuffleElement">
-                    <img src="https://i.imgur.com/Coc4Unz.gif" alt="aaaaaaaaaa"/>
-                </div>
-                <div className="shuffleElement">
-                    <p className="lore">Please refine your search and/or yourself, ye hapless buffoon</p>
-                </div>
-            </div>
-        </div>
-    )
-
     return (
-        <div>
-            {filterSection}
-            <div className="shuffleCards">
-                {filteredItems?.length === 0 ? yeHaplessBuffoon : filteredItems.map((item, index) =>
-                    <div className="shuffleCard shadow-dark"
-                         key={`Card-${item.name}-${item?.type}-${index}`}
-                         style={{borderColor: `${item?.background}`}}>
+      item.name?.toLowerCase().includes(searchText) ||
+      item.title?.toLowerCase().includes(searchText) ||
+      item.description?.join(" ").toLowerCase().includes(searchText) ||
+      item.groupNames?.join(" ").toLowerCase().includes(searchText));
+  }
 
-                        <div className={`shadow-dark rarityBanner rarity-${item?.rarity}`}>
-                            <a className="bannerHitbox" data-tooltip={`Rarity: ${item?.rarity}`}/>
-                            <p></p>
-                        </div>
+  const forceTag = (tag) => {
+    /*
+    if (searchTags.includes(tag)) return;
+    const newTags = Object.assign([], searchTags);
+    newTags.push(tag);
+    setSearchTags(newTags);
+    setSearchText("");
+    setFilter({...filter, type: undefined})
+    */
+  }
 
-                        {item.specialFlag ?
-                            <div className={`shadow-dark specialBanner special-${item.specialFlag}`}>
-                                <a className="bannerHitbox" data-tooltip={`${bannerText[item?.specialFlag]}`}/>
-                                <p></p>
-                            </div> : undefined}
+  const clearAllFilters = () => {
+    setSelectedTags([]);
+    setSelectedType(null);
+    setSelectedRarity(null);
+    setSearchText("");
+  }
 
-                        <div className="shuffleContent">
-                            <div className="shuffleElement">
-                                <McText className="subtitle shuffleElement" prefix={"&"}>{item?.name}</McText>
-                                <div>
-                                    {item?.groupNames?.map((tag, index2) =>
-                                        <button
-                                            className="tag"
-                                            key={`tag${index2}`}
-                                            onClick={() => forceTag(tag)}
-                                        >{tag}</button>)}
-                                </div>
-                                <div className="shuffleElement">
-                                    <img src={item?.img} alt="Loading..."/>
-                                </div>
-                                <div style={{backgroundColor: "black", borderRadius: "5px", margin: "0px 10px"}}>
-                                    <div className="shuffleElement">
-                                        {item?.description?.map((line, index2) => (
-                                            <div>
-                                                <McText
-                                                    className="lore"
-                                                    prefix={"&"}
-                                                    key={`lore${index2}`}>
-                                                    {line}
-                                                </McText>
-                                            </div>)
-                                        )}
-                                    </div>
-                                </div>
-                                <div style={{marginTop: '10px'}}/>
-                            </div>
-                        </div>
-                    </div>
-                )}
-            </div>
+  const filterSection = (
+    <div className="width100 flexRow align-left padding-full-10">
+      <div className="width40 margin-full-5">
+        <Select
+          placeholder="Item Tags"
+          isMulti
+          value={selectedTags}
+          options={filterTags}
+          onChange={setSelectedTags}
+        />
+      </div>
+      <div className="width15 margin-full-5">
+        <Select
+          placeholder="Type"
+          value={selectedType}
+          options={typeOptions}
+          onChange={setSelectedType}
+        />
+      </div>
+      <div className="width15 margin-full-5">
+        <Select
+          placeholder="Rarity"
+          value={selectedRarity}
+          options={rarityOptions}
+          onChange={setSelectedRarity}
+        />
+      </div>
+      <div className="width20 margin-full-5 searchBox">
+        <DebounceInput
+          placeholder="Search Items..."
+          debounceTimeout={1000}
+          value={searchText}
+          forceNotifyByEnter={true}
+          forceNotifyOnBlur={true}
+          minLength={0}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+      </div>
+      <div className="width5 margin-full-5 clearButton align-center"
+           onClick={clearAllFilters}>
+        X</div>
+    </div>
+  )
+
+  const yeHaplessBuffoon = (
+    <div className="shuffleCard" style={{borderColor: "#AA0000"}} key="invalid-search">
+      <div className="shuffleContent">
+        <div className="shuffleElement">
+          <div className="title">⚠ IMPOTENT QUERIER DETECTED ⚠</div>
+          <div className="subtitle">HALT! YOU'VE FOUND NO RESULTS!</div>
         </div>
-    )
+        <div className="shuffleElement">
+          <img src="https://i.imgur.com/Coc4Unz.gif" alt="aaaaaaaaaa"/>
+        </div>
+        <div className="shuffleElement">
+          <p className="lore">Please refine your search and/or yourself, ye hapless buffoon</p>
+        </div>
+      </div>
+    </div>
+  )
+
+  return (
+    <div>
+      {filterSection}
+      <div className="shuffleCards">
+        {filteredItems?.length === 0 ? yeHaplessBuffoon : filteredItems.map((item, index) =>
+          <div className="shuffleCard shadow-dark" id={`card-${index}`}
+               key={`Card-${item.name}-${item?.type}-${index}`}
+               style={{borderColor: `${item?.background}`}}>
+
+            <div className={`shadow-dark rarityBanner rarity-${item?.rarity}`}>
+              <a className="bannerHitbox" data-tooltip={`Rarity: ${item?.rarity}`}/>
+              <p></p>
+            </div>
+
+            {item.specialFlag ?
+              <div className={`shadow-dark specialBanner special-${item.specialFlag}`}>
+                <a className="bannerHitbox" data-tooltip={`${bannerText[item?.specialFlag]}`}/>
+                <p></p>
+              </div> : undefined}
+
+            <div className="shuffleContent">
+              <div className="shuffleElement">
+                <McText className="subtitle shuffleElement" prefix={"&"}>{item?.name}</McText>
+                <div>
+                  {item?.groupNames?.map((tag, index2) =>
+                    <button
+                      className="tag"
+                      key={`tag${index2}`}
+                      onClick={() => forceTag(tag)}
+                    >{tag}</button>)}
+                </div>
+                <div className="shuffleElement">
+                  <img src={item?.img} alt="Loading..."/>
+                </div>
+                <div style={{backgroundColor: "black", borderRadius: "5px", margin: "0px 10px"}}>
+                  <div className="shuffleElement">
+                    {item?.description?.map((line, index2) => (
+                      <div>
+                        <McText
+                          className="lore"
+                          prefix={"&"}
+                          key={`lore${index2}`}>
+                          {line}
+                        </McText>
+                      </div>)
+                    )}
+                  </div>
+                </div>
+                <div style={{marginTop: '10px'}}/>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
 };
