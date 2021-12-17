2386287afbb141cbb84bb172e75bf91aa8acddc5
Faceguy <kattreinkyle@gmail.com>
2021-12-17 18:17:21 -0500

    Discord redirect if connecting with discord subdomain

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.scss';
 
 import React from 'react';
 import {
-    HashRouter as Router,
-    Switch,
-    Route
+  HashRouter as Router,
+  Switch,
+  Route
 } from "react-router-dom";
 import Store from './Store'
 
@@ -17,38 +17,47 @@ import {DiscordWidget} from "./components/DiscordWidget/DiscordWidget"
 import {Guide} from "./pages/Guide/Guide";
 
 function App() {
-
-    return (
-            <Router>
-                <Store>
-                    <MobileStateHandler/>
-                    <Switch>
-                        <Route path="/about">
-                            <About/>
-                        </Route>
-                        <Route path="/guide">
-                            <Guide/>
-                        </Route>
-                        <Route path="/">
-                            <Home/>
-                        </Route>
-                    </Switch>
-                </Store>
-            </Router>
-    );
+  // Handle discord subdomain, redirect to invite
+  let host = window.location.host;
+  let parts = host.split(".");
+  if (parts.length >= 2) {
+    if (parts[0] === "discord") {
+      window.location.href = 'https://discord.gg/VUkE7Db4a8';
+      return <div>Redirecting To Discord...</div>
+    }
+  }
+  return (
+    <Router>
+      <Store>
+        <MobileStateHandler/>
+        <Switch>
+          <Route path="/about">
+            <About/>
+          </Route>
+          <Route path="/guide">
+            <Guide/>
+          </Route>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+      </Store>
+    </Router>
+  );
 }
 
 function About() {
-    return (
-        <div className="App">
-            <HeaderBar fancy={false}/>
-            <div className="basicPage pixelImage" style={{marginTop: "50px", backgroundImage: "url(https://i.imgur.com/rb0H7eH.png)", backgroundSize: 64}}>
-                <ShuffleCollection/>
-            </div>
-            <DiscordWidget/>
-            <Footer/>
-        </div>
-    )
+  return (
+    <div className="App">
+      <HeaderBar fancy={false}/>
+      <div className="basicPage pixelImage"
+           style={{marginTop: "50px", backgroundImage: "url(https://i.imgur.com/rb0H7eH.png)", backgroundSize: 64}}>
+        <ShuffleCollection/>
+      </div>
+      <DiscordWidget/>
+      <Footer/>
+    </div>
+  )
 }
 
 export default App;
