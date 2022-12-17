9ab7a8d67de864258b61644256acffeecce01454
Faceguy <kattreinkyle@gmail.com>
2022-12-17 01:58:23 -0500

    Updating footer

--- src/components/Buddy/Buddy.js
+++ src/components/Buddy/Buddy.js
@@ -6,136 +6,136 @@ import boopSfx from '../../sounds/Player_hurt1.ogg';
 
 export const Buddy = () => {
 
-    const [life, setLife] = useState(Math.random() < 0.05 ? 5 : 0);
-    const [shake, setShake] = useState(false)
+  const [life, setLife] = useState(Math.random() < 1 ? 5 : 0);
+  const [shake, setShake] = useState(false)
 
-    const [play] = useSound(boopSfx);
+  const [play] = useSound(boopSfx);
 
-    const [x, setX] = useState()
-    const [y, setY] = useState()
-    const [hitMarkers, setHitMarkers] = useState([]);
+  const [x, setX] = useState()
+  const [y, setY] = useState()
+  const [hitMarkers, setHitMarkers] = useState([]);
 
-    let shakeTimeout;
-    let hitTimer;
+  let shakeTimeout;
+  let hitTimer;
 
-    useEffect(() => {
-        tickMarkers()
-        return () => {
-            hitTimer && clearTimeout(hitTimer);
-            setHitMarkers([])
-        };
-    }, [setHitMarkers]);
-
-    useEffect(() => {
-            const update = (e) => {
-                setX(e.x)
-                setY(e.y)
-            }
-            window.addEventListener('mousemove', update)
-            window.addEventListener('touchmove', update)
-            return () => {
-                window.removeEventListener('mousemove', update)
-                window.removeEventListener('touchmove', update)
-            }
-        }, [setX, setY]
-    )
-
-    const markerRef = useRef(hitMarkers);
-    markerRef.current = hitMarkers;
-
-    const tickMarkers = () => {
-        if (markerRef.current.length === 0 && life === 0) {
-            return
-        }
-        hitTimer = setTimeout(() => {
-            if (markerRef.current.length > 0) {
-                let newValues = []
-                markerRef.current.forEach(newMarker => {
-                    if (newMarker.lifespan > 0) {
-                        newMarker.x = newMarker.x + newMarker.xVelocity;
-                        newMarker.y = newMarker.y + newMarker.yVelocity;
-                        newMarker.yVelocity = newMarker.yVelocity + 20;
-                        newMarker.lifespan--;
-                        newValues.push(newMarker);
-                    }
-                });
-                setHitMarkers(newValues)
-            }
-            tickMarkers()
-        }, 100)
-    }
-
-    const playSound = () => {
-        play();
+  useEffect(() => {
+    tickMarkers()
+    return () => {
+      hitTimer && clearTimeout(hitTimer);
+      setHitMarkers([])
     };
+  }, [setHitMarkers]);
 
-    const doShake = () => {
-        setShake(true)
-        shakeTimeout && clearTimeout(shakeTimeout);
-        shakeTimeout = setTimeout(() => {
-            setShake(false);
-        }, 100)
+  useEffect(() => {
+      const update = (e) => {
+        setX(e.x)
+        setY(e.y)
+      }
+      window.addEventListener('mousemove', update)
+      window.addEventListener('touchmove', update)
+      return () => {
+        window.removeEventListener('mousemove', update)
+        window.removeEventListener('touchmove', update)
+      }
+    }, [setX, setY]
+  )
+
+  const markerRef = useRef(hitMarkers);
+  markerRef.current = hitMarkers;
+
+  const tickMarkers = () => {
+    if (markerRef.current.length === 0 && life === 0) {
+      return
     }
+    hitTimer = setTimeout(() => {
+      if (markerRef.current.length > 0) {
+        let newValues = []
+        markerRef.current.forEach(newMarker => {
+          if (newMarker.lifespan > 0) {
+            newMarker.x = newMarker.x + newMarker.xVelocity;
+            newMarker.y = newMarker.y + newMarker.yVelocity;
+            newMarker.yVelocity = newMarker.yVelocity + 20;
+            newMarker.lifespan--;
+            newValues.push(newMarker);
+          }
+        });
+        setHitMarkers(newValues)
+      }
+      tickMarkers()
+    }, 100)
+  }
 
-    const createHitMarker = (id) => {
-        const hitMarker = {id: id};
-        hitMarker.lifespan = 10;
-        hitMarker.x = x;
-        hitMarker.y = y;
-        hitMarker.xVelocity = 80 * (-0.5 + Math.random());
-        hitMarker.yVelocity = -30 - 50 * Math.random();
-        hitMarker.text = 8 + Math.floor(Math.random() * Math.floor(22))
+  const playSound = () => {
+    play();
+  };
 
-        const newMarkers = hitMarkers;
-        newMarkers.push(hitMarker);
-        setHitMarkers(newMarkers);
-        console.log(hitMarkers)
-    }
+  const doShake = () => {
+    setShake(true)
+    shakeTimeout && clearTimeout(shakeTimeout);
+    shakeTimeout = setTimeout(() => {
+      setShake(false);
+    }, 100)
+  }
 
-    if (life < 1) {
-        return (
-            <div>
-                {hitMarkers.map((marker, index) =>
-                    <div
-                        id={`marker-${marker.id}`}
-                        key={`marker-${marker.id}`}
-                        className="hit-marker no-select"
-                        style={{
-                            top: marker.y,
-                            left: marker.x
-                        }}>
-                        {marker.text}
-                    </div>
-                )}
-            </div>
-        )
-    }
+  const createHitMarker = (id) => {
+    const hitMarker = {id: id};
+    hitMarker.lifespan = 10;
+    hitMarker.x = x;
+    hitMarker.y = y;
+    hitMarker.xVelocity = 80 * (-0.5 + Math.random());
+    hitMarker.yVelocity = -30 - 50 * Math.random();
+    hitMarker.text = 8 + Math.floor(Math.random() * Math.floor(22))
 
+    const newMarkers = hitMarkers;
+    newMarkers.push(hitMarker);
+    setHitMarkers(newMarkers);
+    console.log(hitMarkers)
+  }
+
+  if (life < 1) {
     return (
-        <div>
-            {hitMarkers.map((marker, index) =>
-                <div
-                    id={`marker-${marker.id}`}
-                    key={`marker-${marker.id}`}
-                    className="hit-marker no-select"
-                    style={{
-                        top: marker.y,
-                        left: marker.x
-                    }}>
-                    {marker.text}
-                </div>
-            )}
-            <img
-                className={`buddy ${shake ? "hitShake" : ""}`}
-                src="https://i.imgur.com/2kT4dcX.png"
-                alt="facebuddy"
-                onClick={() => {
-                    doShake()
-                    createHitMarker(life)
-                    playSound();
-                    setLife(life - 1);
-                }}
-            />
+      <div>
+        {hitMarkers.map((marker, index) =>
+          <div
+            id={`marker-${marker.id}`}
+            key={`marker-${marker.id}`}
+            className="hit-marker no-select"
+            style={{
+              top: marker.y,
+              left: marker.x
+            }}>
+            {marker.text}
+          </div>
+        )}
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      {hitMarkers.map((marker, index) =>
+        <div
+          id={`marker-${marker.id}`}
+          key={`marker-${marker.id}`}
+          className="hit-marker no-select"
+          style={{
+            top: marker.y,
+            left: marker.x
+          }}>
+          {marker.text}
         </div>
-    );
+      )}
+      <img
+        className={`buddy ${shake ? "hitShake" : ""}`}
+        src="https://i.imgur.com/GY83tsk.png"
+        alt="facebuddy"
+        onClick={() => {
+          doShake()
+          createHitMarker(life)
+          playSound();
+          setLife(life - 1);
+        }}
+      />
+    </div>
+  );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,43 +1,76 @@
 import * as React from "react";
 import './footer.scss'
 import {Buddy} from "../Buddy/Buddy";
+import {useContext} from "react";
+import {Context} from "../../Store";
 
 export const Footer = (props) => {
 
-    return (
-        <div className="Footer">
-            <Buddy/>
-            <div className="footer">
-                <div className="flexRow width100">
-                    <div className="footerSection flexCol width40">
-                        <img className="pixelImage width40" style={{opacity: 0.4}} src="https://i.imgur.com/0gTVxe3.png"
-                             alt="emboss_logo"/>
-                    </div>
-                    <div className="footerSection flexCol width60">
-                        <div className="flexRow width100">
-                            <div className="flexCol width50" style={{alignItems: "flex-start"}}>
-                                <h4 className="thin-underline">GAMERS</h4>
-                                <div>Home</div>
-                                <div>Info</div>
-                                <div>Contact</div>
-                                <div>Discord</div>
-                                <div>Privacy Policy</div>
-                            </div>
-                            <div className="flexCol width50" style={{alignItems: "flex-start"}}>
-                                <h4 className="thin-underline">GAMERS</h4>
-                                <div>Home</div>
-                                <div>Info</div>
-                                <div>Contact</div>
-                                <div>Discord</div>
-                                <div>Privacy Policy</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="flexRow width100">
-                    COPYRIGHT YOUR MOTHER LMFAO
-                </div>
-            </div>
+  const [state] = useContext(Context);
+
+  return (
+    <div className="Footer">
+      <Buddy/>
+      <footer className="footer">
+        <div className="footer__addr">
+          <h1 className="footer__logo">Faceland RPG</h1>
+
+          <address>
+            1234 ImNotGivingYouWeirdosMyAddress Blvd 42069
+            <p>&copy; 2023 Faceland LLC. All rights reserved.</p>
+          </address>
         </div>
-    )
+
+        <ul className="footer__nav">
+
+          <li className="nav__item">
+            <h2 className="nav__title">Social Media</h2>
+
+            <ul className="nav__ul">
+              <li>
+                <a href="https://twitter.com/facelandr">Twitter</a>
+              </li>
+
+              <li>
+                <a href="https://twitter.com/facelandr">Still Twitter</a>
+              </li>
+
+              <li>
+                <a href="https://twitter.com/facelandr">Send help, it's just Twitter</a>
+              </li>
+            </ul>
+          </li>
+
+          <li className="nav__item">
+            <h2 className="nav__title">External Links</h2>
+
+            <ul className="nav__ul">
+              <li>
+                <a href="http://199.127.61.235:8100/#Quest_world:27:0:893:506:0:0:0:0:perspective">World Map</a>
+              </li>
+
+              <li>
+                <a href="https://discord.gg/VUkE7Db4a8">Discord</a>
+              </li>
+
+              <li>
+                <a href="http://shop.face.land/category/336956">Cash Shop</a>
+              </li>
+            </ul>
+          </li>
+
+          <li className="nav__item">
+            <h2 className="nav__title">Contact</h2>
+
+            <ul className="nav__ul">
+              <li>
+                <a className="footer__btn" href="mailto:official.faceland@gmail.com">Email Us</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+
+      </footer>
+    </div>
+  )
 }
