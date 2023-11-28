f54a897a768c481b3282f2d793d40f91a3854878
DESKTOP-9N26LGA\Also Rachel <racheladelle@pm.me>
2023-11-28 12:58:30 -0500

    add tailwind, autoformat ShuffleCollection

--- src/components/Shuffle/ShuffleCollection.js
+++ src/components/Shuffle/ShuffleCollection.js
@@ -1,82 +1,90 @@
-import React, {useState, useEffect, useContext} from "react";
-import Select from "react-select";
-import "react-select/dist/react-select.cjs";
-import "./Shuffle.scss"
-import "../Tooltip/tooltip.scss"
-import gems from "./gems.json"
-import tomes from "./tomes.json"
-import uniques from "./uniques.json"
-import scrolls from "./scrolls.json"
-import McText from "mctext-react/lib/McText";
-import {DebounceInput} from "react-debounce-input";
-import {Context} from "../../Store";
+import React, { useState, useEffect, useContext } from 'react';
+import Select from 'react-select';
+import 'react-select/dist/react-select.cjs';
+import './Shuffle.scss';
+import '../Tooltip/tooltip.scss';
+import gems from './gems.json';
+import tomes from './tomes.json';
+import uniques from './uniques.json';
+import scrolls from './scrolls.json';
+import McText from 'mctext-react/lib/McText';
+import { DebounceInput } from 'react-debounce-input';
+import { Context } from '../../Store';
 
 export const ShuffleCollection = () => {
-
   const [state] = useContext(Context);
 
   const [cardItems, setCardItems] = useState([]);
-  const [filteredItems, setFilteredItems] = useState([{name: "loading", meme: "yes"}]);
+  const [filteredItems, setFilteredItems] = useState([
+    { name: 'loading', meme: 'yes' },
+  ]);
 
   const [availableTags, setAvailableTags] = useState([]);
 
   const [selectedTags, setSelectedTags] = useState([]);
-  const [selectedType, setSelectedType] = useState({value: "gem", label: "Gem"});
+  const [selectedType, setSelectedType] = useState({
+    value: 'gem',
+    label: 'Gem',
+  });
   const [selectedRarity, setSelectedRarity] = useState();
   const [searchText, setSearchText] = useState();
 
   const [filterTags, setFilterTags] = useState([]);
 
   const bannerText = {};
-  bannerText.event = "Event Reward"
-  bannerText.transmute = "Transmutation Only"
-  bannerText.discontinued = "Discontinued"
+  bannerText.event = 'Event Reward';
+  bannerText.transmute = 'Transmutation Only';
+  bannerText.discontinued = 'Discontinued';
 
   const typeOptions = [
-    {value: "any", label: "Any"},
-    {value: "gem", label: "Gem"},
-    {value: "tome", label: "Tome"},
-    {value: "scroll", label: "Scroll"},
-    {value: "unique", label: "Unique"}
-  ]
+    { value: 'any', label: 'Any' },
+    { value: 'gem', label: 'Gem' },
+    { value: 'tome', label: 'Tome' },
+    { value: 'scroll', label: 'Scroll' },
+    { value: 'unique', label: 'Unique' },
+  ];
 
   const rarityOptions = [
-    {value: "any", label: "Any"},
-    {value: "Common", label: "Common"},
-    {value: "Uncommon", label: "Uncommon"},
-    {value: "Rare", label: "Rare"},
-    {value: "Epic", label: "Epic"}
-  ]
+    { value: 'any', label: 'Any' },
+    { value: 'Common', label: 'Common' },
+    { value: 'Uncommon', label: 'Uncommon' },
+    { value: 'Rare', label: 'Rare' },
+    { value: 'Epic', label: 'Epic' },
+  ];
 
   useEffect(() => {
     if (cardItems.length === 0) {
       const newItems = [];
       for (const [key, value] of Object.entries(gems)) {
         const item = value;
-        item.type = "gem"
-        item.img = "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/26/Emerald_JE3_BE3.png";
-        item.background = "#10c810";
+        item.type = 'gem';
+        item.img =
+          'https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/26/Emerald_JE3_BE3.png';
+        item.background = '#10c810';
         newItems.push(item);
       }
       for (const [key, value] of Object.entries(tomes)) {
         const item = value;
-        item.type = "tome";
-        item.img = "https://static.wikia.nocookie.net/minecraft_gamepedia/images/5/50/Book_JE2_BE2.png";
-        item.background = "#1243d9";
+        item.type = 'tome';
+        item.img =
+          'https://static.wikia.nocookie.net/minecraft_gamepedia/images/5/50/Book_JE2_BE2.png';
+        item.background = '#1243d9';
         newItems.push(item);
       }
       for (const [key, value] of Object.entries(uniques)) {
         const item = value;
-        item.type = "unique";
-        item.img = "https://static.wikia.nocookie.net/minecraft_gamepedia/images/5/50/Book_JE2_BE2.png";
-        item.background = "#d99712";
+        item.type = 'unique';
+        item.img =
+          'https://static.wikia.nocookie.net/minecraft_gamepedia/images/5/50/Book_JE2_BE2.png';
+        item.background = '#d99712';
         newItems.push(item);
       }
       for (const [key, value] of Object.entries(scrolls)) {
         const item = value;
-        item.type = "scroll";
-        item.img = "https://static.wikia.nocookie.net/minecraft_gamepedia/images/f/f2/Paper_JE2_BE2.png";
-        item.background = "#34981a";
+        item.type = 'scroll';
+        item.img =
+          'https://static.wikia.nocookie.net/minecraft_gamepedia/images/f/f2/Paper_JE2_BE2.png';
+        item.background = '#34981a';
         newItems.push(item);
       }
 
@@ -86,7 +94,7 @@ export const ShuffleCollection = () => {
   }, []);
 
   useEffect(() => {
-    applyFilters()
+    applyFilters();
   }, [searchText, selectedTags, selectedRarity, selectedType]);
 
   useEffect(() => {
@@ -94,13 +102,13 @@ export const ShuffleCollection = () => {
     cardItems.forEach((item) => {
       item.groupNames.forEach((tag) => {
         set.add(tag);
-      })
+      });
     });
     setAvailableTags(Array.from(set));
 
-    const tags = []
+    const tags = [];
     set.forEach((tag) => {
-      tags.push({value: tag, label: tag})
+      tags.push({ value: tag, label: tag });
     });
     setFilterTags(tags);
   }, [cardItems]);
@@ -108,42 +116,60 @@ export const ShuffleCollection = () => {
   const applyFilters = () => {
     setFilteredItems(
       cardItems
-        .filter(item => itemMatchesFilters(item) && tagsApplicable(item) && itemMatchesSearch(item))
+        .filter(
+          (item) =>
+            itemMatchesFilters(item) &&
+            tagsApplicable(item) &&
+            itemMatchesSearch(item),
+        )
         .sort(function (a, b) {
-          return a.strippedName < b.strippedName ? -1 : a.strippedName > b.strippedName ? 1 : 0;
-        })
+          return a.strippedName < b.strippedName
+            ? -1
+            : a.strippedName > b.strippedName
+              ? 1
+              : 0;
+        }),
     );
-  }
+  };
 
   const itemMatchesFilters = (item) => {
-    if (selectedRarity && selectedRarity.value !== "any" && item.rarity !== selectedRarity.value) {
+    if (
+      selectedRarity &&
+      selectedRarity.value !== 'any' &&
+      item.rarity !== selectedRarity.value
+    ) {
       return false;
     }
-    if (selectedType && selectedType.value !== "any" && item.type !== selectedType.value) {
+    if (
+      selectedType &&
+      selectedType.value !== 'any' &&
+      item.type !== selectedType.value
+    ) {
       return false;
     }
-    return true
-  }
+    return true;
+  };
 
   const tagsApplicable = (item) => {
     if (selectedTags.length === 0) return true;
     for (const tag of selectedTags) {
-      if (!item.groupNames.includes(tag.value)) return false
+      if (!item.groupNames.includes(tag.value)) return false;
     }
     return true;
-  }
+  };
 
   const itemMatchesSearch = (item) => {
     if (!searchText) {
       return true;
     }
-    const lowered = searchText.toLowerCase()
+    const lowered = searchText.toLowerCase();
     return (
       item.name?.toLowerCase().includes(lowered) ||
       item.title?.toLowerCase().includes(lowered) ||
-      item.description?.join(" ").toLowerCase().includes(lowered) ||
-      item.groupNames?.join(" ").toLowerCase().includes(lowered));
-  }
+      item.description?.join(' ').toLowerCase().includes(lowered) ||
+      item.groupNames?.join(' ').toLowerCase().includes(lowered)
+    );
+  };
 
   const forceTag = (newTag) => {
     let exists = false;
@@ -156,16 +182,16 @@ export const ShuffleCollection = () => {
       return;
     }
     const newTags = Object.assign([], selectedTags);
-    newTags.push({value: newTag, label: newTag});
+    newTags.push({ value: newTag, label: newTag });
     setSelectedTags(newTags);
-  }
+  };
 
   const clearAllFilters = () => {
     setSelectedTags([]);
     setSelectedType(null);
     setSelectedRarity(null);
-    setSearchText("");
-  }
+    setSearchText('');
+  };
 
   const desktopFilterSection = (
     <div className="flexRow align-left padding-full-15">
@@ -211,13 +237,12 @@ export const ShuffleCollection = () => {
       </div>
       <div className="width5">
         <span className="filterTitles">Clear</span>
-        <div className="clearButton align-center"
-             onClick={clearAllFilters}>
+        <div className="clearButton align-center" onClick={clearAllFilters}>
           X
         </div>
       </div>
     </div>
-  )
+  );
 
   const mobileFilterSection = (
     <>
@@ -267,86 +292,118 @@ export const ShuffleCollection = () => {
       </div>
       <div className="flexRow align-left padding-full-15">
         <div className="width100">
-          <div className="clearButton align-center"
-               onClick={clearAllFilters}>
+          <div className="clearButton align-center" onClick={clearAllFilters}>
             X
           </div>
         </div>
       </div>
     </>
-  )
+  );
 
   const yeHaplessBuffoon = (
-    <div className="shuffleCard" style={{borderColor: "#AA0000"}} key="invalid-search">
+    <div
+      className="shuffleCard"
+      style={{ borderColor: '#AA0000' }}
+      key="invalid-search"
+    >
       <div className="shuffleContent">
         <div className="shuffleElement">
           <div className="title">⚠ IMPOTENT QUERIER DETECTED ⚠</div>
           <div className="subtitle">HALT! YOU'VE FOUND NO RESULTS!</div>
         </div>
         <div className="shuffleElement">
-          <img src="https://i.imgur.com/eb3dM.gif" alt="aaaaaaaaaa"/>
+          <img src="https://i.imgur.com/eb3dM.gif" alt="aaaaaaaaaa" />
         </div>
         <div className="shuffleElement">
-          <p className="lore">Please refine your search and/or yourself, ye hapless buffoon</p>
+          <p className="lore">
+            Please refine your search and/or yourself, ye hapless buffoon
+          </p>
         </div>
       </div>
     </div>
-  )
+  );
 
   return (
     <div>
       {state.mobile ? mobileFilterSection : desktopFilterSection}
       <div className="shuffleCards">
-        {filteredItems?.length === 0 ? yeHaplessBuffoon : filteredItems.map((item, index) =>
-          <div className="shuffleCard shadow-dark" id={`card-${index}`}
-               key={`Card-${item.name}-${item?.type}-${index}`}
-               style={{borderColor: `${item?.background}`}}>
-
-            <div className={`shadow-dark rarityBanner rarity-${item?.rarity}`}>
-              <a className="bannerHitbox" data-tooltip={`Rarity: ${item?.rarity}`}/>
-              <p></p>
-            </div>
-
-            {item.specialFlag ?
-              <div className={`shadow-dark specialBanner special-${item.specialFlag}`}>
-                <a className="bannerHitbox" data-tooltip={`${bannerText[item?.specialFlag]}`}/>
-                <p></p>
-              </div> : undefined}
-
-            <div className="shuffleContent">
-              <div className="shuffleElement">
-                <McText className="subtitle shuffleElement" prefix={"&"}>{item?.name}</McText>
-                <div>
-                  {item?.groupNames?.map((tag, index2) =>
-                    <button
-                      className="tag"
-                      key={`tag${index2}`}
-                      onClick={() => forceTag(tag)}
-                    >{tag}</button>)}
+        {filteredItems?.length === 0
+          ? yeHaplessBuffoon
+          : filteredItems.map((item, index) => (
+              <div
+                className="shuffleCard shadow-dark"
+                id={`card-${index}`}
+                key={`Card-${item.name}-${item?.type}-${index}`}
+                style={{ borderColor: `${item?.background}` }}
+              >
+                <div
+                  className={`shadow-dark rarityBanner rarity-${item?.rarity}`}
+                >
+                  <a
+                    className="bannerHitbox"
+                    data-tooltip={`Rarity: ${item?.rarity}`}
+                  />
+                  <p></p>
                 </div>
-                <div className="shuffleElement">
-                  <img src={item?.img} alt="Loading..."/>
-                </div>
-                <div style={{backgroundColor: "black", borderRadius: "5px", margin: "0px 10px"}}>
+
+                {item.specialFlag ? (
+                  <div
+                    className={`shadow-dark specialBanner special-${item.specialFlag}`}
+                  >
+                    <a
+                      className="bannerHitbox"
+                      data-tooltip={`${bannerText[item?.specialFlag]}`}
+                    />
+                    <p></p>
+                  </div>
+                ) : undefined}
+
+                <div className="shuffleContent">
                   <div className="shuffleElement">
-                    {item?.description?.map((line, index2) => (
-                      <div>
-                        <McText
-                          className="lore"
-                          prefix={"&"}
-                          key={`lore${index2}`}>
-                          {line}
-                        </McText>
-                      </div>)
-                    )}
+                    <McText className="subtitle shuffleElement" prefix={'&'}>
+                      {item?.name}
+                    </McText>
+                    <div>
+                      {item?.groupNames?.map((tag, index2) => (
+                        <button
+                          className="tag"
+                          key={`tag${index2}`}
+                          onClick={() => forceTag(tag)}
+                        >
+                          {tag}
+                        </button>
+                      ))}
+                    </div>
+                    <div className="shuffleElement">
+                      <img src={item?.img} alt="Loading..." />
+                    </div>
+                    <div
+                      style={{
+                        backgroundColor: 'black',
+                        borderRadius: '5px',
+                        margin: '0px 10px',
+                      }}
+                    >
+                      <div className="shuffleElement">
+                        {item?.description?.map((line, index2) => (
+                          <div>
+                            <McText
+                              className="lore"
+                              prefix={'&'}
+                              key={`lore${index2}`}
+                            >
+                              {line}
+                            </McText>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                    <div style={{ marginTop: '10px' }} />
                   </div>
                 </div>
-                <div style={{marginTop: '10px'}}/>
               </div>
-            </div>
-          </div>
-        )}
+            ))}
       </div>
     </div>
-  )
+  );
 };
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@material-tailwind/react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
