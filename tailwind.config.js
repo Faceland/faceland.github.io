619061ad41aae09e69150123147ae99d141aae66
DESKTOP-9N26LGA\Also Rachel <racheladelle@pm.me>
2023-11-28 20:00:58 -0500

    style new item cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ const withMT = require('@material-tailwind/react/utils/withMT');
 module.exports = withMT({
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      'black-forest': '#081104',
+      chambray: '#3B5998',
+      'murder-brown': '#090A20',
+      kilamanjaro: '#1E0802',
+      'san-marino': '#4C6DB1',
+    },
     extend: {},
   },
   plugins: [],
