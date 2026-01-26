12b14f16e2df5701c202c2c1f5906e1b3a4516e1
Faceguy <kattreinkyle@gmail.com>
2026-01-26 00:26:17 -0500

    More SEO fixes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { createRoot, hydrateRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ThemeProvider } from '@material-tailwind/react';
 
-const root = createRoot(document.getElementById('root'));
-root.render(
+const rootElement = document.getElementById('root');
+
+const app = (
   <StrictMode>
     <ThemeProvider>
       <App />
     </ThemeProvider>
-  </StrictMode>,
+  </StrictMode>
 );
+
+// If the page was pre-rendered by react-snap, hydrate instead of render
+if (rootElement.hasChildNodes()) {
+  hydrateRoot(rootElement, app);
+} else {
+  createRoot(rootElement).render(app);
+}
